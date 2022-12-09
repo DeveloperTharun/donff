13d1b0baee9681f3ea575d653da987805dc3cef1
DeveloperTharun <114932079+DeveloperTharun@users.noreply.github.com>
2022-12-09 08:34:12 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -4,20 +4,20 @@ import os
 
 
 class Config(object):
-	API_ID = int(os.environ.get("API_ID", "8754146")
+	API_ID = int(os.environ.get("API_ID", "8754146"))
 	API_HASH = os.environ.get("API_HASH", "8b56a6989f6d04f6f4fe78133ade02fd")
 	BOT_TOKEN = os.environ.get("BOT_TOKEN", "5704245170:AAFx4BGfBcXr1koGb4vnc6zOF33OQmOaV2A")
 	BOT_USERNAME = os.environ.get("BOT_USERNAME", "Sk_File_Store_Free_Bot")
-	DB_CHANNEL = int(os.environ.get("DB_CHANNEL", "-1001642144231")
-	BOT_OWNER = int(os.environ.get("BOT_OWNER", "1632597748")
+	DB_CHANNEL = int(os.environ.get("DB_CHANNEL", "-1001642144231"))
+	BOT_OWNER = int(os.environ.get("BOT_OWNER", "1632597748"))
 	DATABASE_URL = os.environ.get("DATABASE_URL", "mongodb+srv://SkMedia:Tharunraj1828@cluster0.vbdxs.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
 	UPDATES_CHANNEL = os.environ.get("UPDATES_CHANNEL", "-1001708226012")
 	LOG_CHANNEL = os.environ.get("LOG_CHANNEL", "-1001642144231")
 	BANNED_USERS = set(int(x) for x in os.environ.get("BANNED_USERS", "1234567890").split())
-	FORWARD_AS_COPY = bool(os.environ.get("FORWARD_AS_COPY", True)
-	BROADCAST_AS_COPY = bool(os.environ.get("BROADCAST_AS_COPY", False)
-	BANNED_CHAT_IDS = list(set(int(x) for x in os.environ.get("BANNED_CHAT_IDS", "-1001362659779 -1001255795497").split())
-	OTHER_USERS_CAN_SAVE_FILE = bool(os.environ.get("OTHER_USERS_CAN_SAVE_FILE", True)
+	FORWARD_AS_COPY = bool(os.environ.get("FORWARD_AS_COPY", True))
+	BROADCAST_AS_COPY = bool(os.environ.get("BROADCAST_AS_COPY", False))
+	BANNED_CHAT_IDS = list(set(int(x) for x in os.environ.get("BANNED_CHAT_IDS", "-1001362659779 -1001255795497").split()))
+	OTHER_USERS_CAN_SAVE_FILE = bool(os.environ.get("OTHER_USERS_CAN_SAVE_FILE", True))
 	ABOUT_BOT_TEXT = f"""
 This is a Permanent FileStore Bot. 
 Send Me any Media or File.I can Work In Channel too Add Me to Channel with Edit Permission, I will add save Uploaded File in Channel and Share a Shareable Link. 
